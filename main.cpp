eb7580d08c860358632c57c8b49cfb8227822efe
vedhakrisna <73299489+vedhakrisna@users.noreply.github.com>
2021-10-06 22:03:52 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,79 @@
+int main()
+{
+//   int n=0, res=0, res2=0;
+//   cin>>n;
+//   cout << "no of digits = " <<  floor(log10(n)+1);
+
+// for(int i=5;i<n;i=i*5)
+//     res = res + n/i;
+// cout << "total no of zeros in n! = " << res;
+
+// cout<<isPrime(20);
+
+// cout<<kSet(5,2);
+// printNums(5);
+// fun(12);
+
+// while(n>0){
+//     res = res*10 + (n%2);
+//     n=n/2;
+// }
+// cout << res2;
+// cout << fibonacii(4);
+
+// string abc = "abcda";
+// cout << isPallindrome(abc, 0, abc.length()-1); 
+
+// cout << sum_digits(12321);
+
+// int arr[5];
+// for(int i=0;i<4;i++){
+//     arr[i] = i;
+// }
+// insert(arr, 5, 4, 10);
+
+// int arr[] = {10,199,0,210,32, 1024, 2015};
+// cout << max_element(arr, 7)<<endl;
+// cout << sec_max_element(arr, 7);
+
+// int arr[] = {10,199,200,210,320, 1024, 2015};
+// cout << isSorted(arr, 7);
+
+// int arr[] = {10,10,10};
+// remove_duplicates(arr,3);
+
+// int arr[] = {10,0,12,0,1,0,0,5,3};
+// move_zeros_to_last(arr, 9);
+
+// int arr[] = {10,199,200,210,320,1024,2015};
+// circular_left_rotate_by_one(arr, 7);
+// left_rotate_by_one(arr, 7);
+// right_rotate_by_one(arr,7);
+// circular_right_rotate_by_one(arr,7);
+
+// int arr[] = {10,199,200,210,320,1024,2015};
+// cout << binary_search(arr, 7, 199);
+
+
+// int arr[] = {10,199,200,210,320,1024,2015};
+// int start = 0, end = 6;
+// cout << binary_search_recurssion(arr, 7, 240, start, end);
+
+
+// int arr[] = {10,10,10,10,20,20,20};
+// cout<<first_occurence(arr,7,20);
+
+// int arr[] = {10,10,10,10,20,20,20};
+// cout<<last_occurence(arr,7,10);
+
+// int arr[] = {10,10,10,10,20,20,20,30,30,40,40};
+// cout<<total_occurences(arr,sizeof(arr)/sizeof(int),30);
+
+// int arr[] = {0,0,0,0,0,1};
+// cout<<count_ones(arr,sizeof(arr)/sizeof(int));
+
+cout<<square_root(10)<<" "<<square_root(25)<<" "<<square_root(40);
+
+
+return 0;
+}
