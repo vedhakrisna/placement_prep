69722bb61f963ae0d11d590b88a65707c5ea6aef
Vishal S <65108964+blog-a1@users.noreply.github.com>
2021-10-12 17:53:11 +0530

    Added move all zeroes to the end of array

---
+++ move_all_zeroes_to_end_of_array.cpp
@@ -0,0 +1,45 @@
+// https://practice.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1/?category[]=Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    // code here
+	    int count = 0;
+	    for(int i = 0; i < n; i++)
+	    {
+	        if(arr[i] > 0)
+	        {
+	            arr[count] = arr[i];
+	            if(count != i) 
+	                arr[i] = 0;
+	            count++;
+	        }
+	    }
+	}
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.pushZerosToEnd(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
